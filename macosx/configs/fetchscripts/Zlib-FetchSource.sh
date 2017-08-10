d96596548889af232d89507bf30502d6282f9576
Admin2 <admin2@Admin2s-Mac.local>
2017-08-10 13:28:55 -0400

    [mac]: Update zlib to 1.2.11

--- macosx/configs/fetchscripts/Zlib-FetchSource.sh
+++ macosx/configs/fetchscripts/Zlib-FetchSource.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VerLib="1.2.8"
+VerLib="1.2.11"
 OutDir="zlib"
 DirectorY="${OutDir}-${VerLib}"
 FileName="${DirectorY}.tar.gz"
 SourceDLP="http://zlib.net/${FileName}"
-MD5Sum="44d667c142d7cda120332623eab69f40"
+MD5Sum="1c9f62f0778697a09d36121ead88e08e"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
