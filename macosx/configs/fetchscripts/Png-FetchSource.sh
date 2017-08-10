aa09937e1280b5fbcedc0cee04ed926c3893e4b4
Admin2 <admin2@Admin2s-Mac.local>
2017-08-10 13:29:08 -0400

    [mac]: Update libpng to 1.5.28

--- macosx/configs/fetchscripts/Png-FetchSource.sh
+++ macosx/configs/fetchscripts/Png-FetchSource.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VerLib="1.5.21"
+VerLib="1.5.28"
 OutDir="libpng"
 DirectorY="${OutDir}-${VerLib}"
 FileName="${DirectorY}.tar.gz"
 SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng15/${VerLib}/${FileName}"
-MD5Sum="5a399a6dd143cb82cdb6c8d98c75fa42"
+MD5Sum="d0647c75aa4ca62707c91e8445e6886a"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
