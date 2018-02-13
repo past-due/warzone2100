c6a7c4cff7f82ecb0c3e02f62b2d7f7cfe5c331c
past-due <30942300+past-due@users.noreply.github.com>
2018-02-13 01:42:03 -0500

    More tweaks

--- macosx/BuildBot/01_configure.sh
+++ macosx/BuildBot/01_configure.sh
@@ -3,7 +3,12 @@
 # Note:
 # This script is meant to be run from the root of the working copy.
 # 
-# This just tries to download the external libs.  It does it twice because they may not download properly the first time.
+# This script runs autorevision, and tries to download the external libs and sequences.
+# It attempts the download twice because they may not download properly the first time.
+#
+# IMPORTANT:
+# To download the sequences, the environment variable "MAC_BUILD_RELEASE" must be set to "true"
+#
 
 ##############################
 # General Setup
@@ -12,14 +17,42 @@
 
 
 ##############################
-# Download External Libs
+#
 
 cd macosx
 
+# Run autorevision
+if ! execute_xcodebuild_command -project Warzone.xcodeproj -target "Autorevision" -configuration "Release"; then
+	exit ${?}
+fi
+
+# Fetch external libraries
 if ! execute_xcodebuild_command -project Warzone.xcodeproj -target "Fetch Third Party Sources"; then
 	if ! execute_xcodebuild_command -project Warzone.xcodeproj -target "Fetch Third Party Sources" -PBXBuildsContinueAfterErrors=NO; then
 		exit ${?}
 	fi
 fi
+
+. "../src/autorevision.cache"
+if [[ ! "${VCS_TAG}" == *_beta* ]] && [[ ! "${VCS_TAG}" == *_rc* ]]; then
+	if [ -n "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+		# Skip downloading videos for pull requests
+		echo "Skip including video sequences for pull requests."
+		exit 0
+	fi
+	if [ "$MAC_BUILD_RELEASE" != "true" ]; then
+		# Skip downloading videos for non-release commits
+		echo "Skip including video sequences because MAC_BUILD_RELEASE is not set to \"true\"."
+		exit 0
+	fi
+
+	# Should fetch video sequences
+	# For now, specify the standard quality sequence only
+	# And download it directly into the ../data/ folder, so it gets picked up by the Xcode app build
+	if ! configs/FetchVideoSequences.sh standard ../data; then
+		exit ${?}
+	fi
+fi
+
 
 exit 0
--- macosx/BuildBot/03_upload.sh
+++ macosx/BuildBot/03_upload.sh
@@ -3,19 +3,34 @@
 # Note:
 # This script is meant to be run from the root of the working copy.
 # 
-# This script sets the name of the .dmg and dSYM bundle as it uploads them and does some link magic.
+# This script sets the name of the .zip as it uploads it and does some link magic.
 
 # Config
 rtag="$(git branch --no-color | sed -e '/^[^*]/d' -e 's:* \(.*\):\1:')"
 uurl="buildbot@buildbot.wz2100.net"
 opth="${rtag}/mac/"
 rpth="public_html/"
-lpth="macosx/build/dmgout/out/"
+#lpth="macosx/build/dmgout/out/"
 revt="-$(git rev-parse -q --short --verify HEAD)"
-dmg_bn="warzone2100"
-dmg_nv="-novideo.dmg"
-tar_dS="-dSYM.tar.gz"
+#dmg_bn="warzone2100"
+#dmg_nv="-novideo.dmg"
+#tar_dS="-dSYM.tar.gz"
 
+if [ -n "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+	# Skip deploying from pull requests
+	echo "Skip deploy from pull requests."
+	exit 0
+fi
+
+if [ -z "$SECURE_MAC_UPLOAD_BASE64_KEY" ]; then
+	# To deploy builds to the buildbot server, a secure environment variable named
+	# SECURE_MAC_UPLOAD_BASE64_KEY must be set in Travis-CI repo settings.
+	#
+	# Its value should be the base64-encoded private SSH key.
+	#
+	echo "Skip deploy; SECURE_MAC_UPLOAD_BASE64_KEY is not set."
+	exit 0
+fi
 
 # Set bran
 if [ -z ${rtag} ]; then
@@ -25,28 +40,36 @@ fi
 bran="-${rtag}"
 
 
-# Upload the dSYM bundle
-echo "Starting to upload the dSYM bundle."
-if ! scp -pqCl 320 ${lpth}${dmg_bn}${tar_dS} ${uurl}:${rpth}${opth}${dmg_bn}${bran}${revt}${tar_dS}; then
-	exstat="${?}"
-	echo "error: Upload did not complete!"
-	exit ${exstat}
+cd macosx/build/Release
+
+# Check if the .zip file contains a sequences.wz
+unzip -l Warzone.zip | grep -q Warzone.app/Contents/Resources/data/sequences.wz
+if [ "$?" == "0" ]; then
+	# The embedded Warzone.app has a sequences.wz file
 fi
 
-# Upload the .dmg
-echo "Starting to upload the dmg image."
-if ! scp -pqCl 320 ${lpth}${dmg_bn}${dmg_nv} ${uurl}:${rpth}${opth}${dmg_bn}${bran}${revt}.dmg; then
-	exstat="${?}"
-	echo "error: Upload did not complete!"
-	exit ${exstat}
-fi
-
-
-# Link up the current .dmg and dSYM bundle
-if ! ssh ${uurl} -C "cd ${rpth} && ln -fs ${opth}${dmg_bn}${bran}${revt}.dmg ${dmg_bn}${bran}-current.dmg && ln -fs ${opth}${dmg_bn}${bran}${revt}${tar_dS} ${dmg_bn}${bran}-current${tar_dS}"; then
-	exstat="${?}"
-	echo "error: Failed to link!"
-	exit ${exstat}
-fi
+## Upload the dSYM bundle
+#echo "Starting to upload the dSYM bundle."
+#if ! scp -pqCl 320 ${lpth}${dmg_bn}${tar_dS} ${uurl}:${rpth}${opth}${dmg_bn}${bran}${revt}${tar_dS}; then
+#	exstat="${?}"
+#	echo "error: Upload did not complete!"
+#	exit ${exstat}
+#fi
+#
+## Upload the .dmg
+#echo "Starting to upload the dmg image."
+#if ! scp -pqCl 320 ${lpth}${dmg_bn}${dmg_nv} ${uurl}:${rpth}${opth}${dmg_bn}${bran}${revt}.dmg; then
+#	exstat="${?}"
+#	echo "error: Upload did not complete!"
+#	exit ${exstat}
+#fi
+#
+#
+## Link up the current .dmg and dSYM bundle
+#if ! ssh ${uurl} -C "cd ${rpth} && ln -fs ${opth}${dmg_bn}${bran}${revt}.dmg ${dmg_bn}${bran}-current.dmg && ln -fs ${opth}${dmg_bn}${bran}${revt}${tar_dS} ${dmg_bn}${bran}-current${tar_dS}"; then
+#	exstat="${?}"
+#	echo "error: Failed to link!"
+#	exit ${exstat}
+#fi
 
 exit 0
