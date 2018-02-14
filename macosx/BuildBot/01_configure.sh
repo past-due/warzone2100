d54b88a125522f1c1e3568ad0796064aa3c4a932
past-due <30942300+past-due@users.noreply.github.com>
2018-02-14 13:29:01 -0500

    Tweak scripts

--- macosx/BuildBot/01_configure.sh
+++ macosx/BuildBot/01_configure.sh
@@ -40,8 +40,39 @@ echo "VCS_WC_MODIFIED=${VCS_WC_MODIFIED}"
 echo ""
 echo "Additional info:"
 echo "git rev-list --count HEAD => \"$(git rev-list --count HEAD)\""
-echo "git describe --abbrev=0 --tags => \"$(git describe --abbrev=0 --tags)\""
+echo "git describe --abbrev=0 --tags 2> /dev/null => \"$(git describe --abbrev=0 --tags 2> /dev/null)\""
 echo "."
+
+# See: https://stackoverflow.com/a/44036486
+function create_all_branches()
+{
+    # Keep track of where Travis put us.
+    # We are on a detached head, and we need to be able to go back to it.
+    local build_head=$(git rev-parse HEAD)
+
+    # Fetch all the remote branches. Travis clones with `--depth`, which
+    # implies `--single-branch`, so we need to overwrite remote.origin.fetch to
+    # do that.
+    git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+    git fetch
+    # optionally, we can also fetch the tags
+    git fetch --tags
+
+#    # create the tacking branches
+#    for branch in $(git branch -r|grep -v HEAD) ; do
+#        git checkout -qf ${branch#origin/}
+#    done
+
+    # finally, go back to where we were at the beginning
+    git checkout ${build_head}
+}
+
+create_all_branches
+echo "Additional info [2]:"
+echo "git rev-list --count HEAD => \"$(git rev-list --count HEAD)\""
+echo "git describe --abbrev=0 --tags 2> /dev/null => \"$(git describe --abbrev=0 --tags 2> /dev/null)\""
+echo "."
+
 
 # Fetch external libraries
 if ! execute_xcodebuild_command -project Warzone.xcodeproj -target "Fetch Third Party Sources"; then
