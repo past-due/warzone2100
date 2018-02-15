17a1051e33ed97fd91990f788e687abb9639ee80
past-due <30942300+past-due@users.noreply.github.com>
2018-02-14 22:57:49 -0500

    Tweak output formatting

--- macosx/BuildBot/03_compile.sh
+++ macosx/BuildBot/03_compile.sh
@@ -49,26 +49,26 @@ codesign_verify_result=$(codesign --verify --deep --strict --verbose=2 Warzone.a
 echo "  -> codesign --verify --deep --strict --verbose=2 Warzone.app"
 if [ -n "${codesign_verify_result}" ]; then
 	while read -r line; do
-		echo "    $line"
+		echo "     $line"
 	done <<< "$codesign_verify_result"
 else
-	echo "    (No output?)"
+	echo "     (No output?)"
 fi
 
 
-echo "Creating Warzone.zip..."
 if [ -n "$TRAVIS" ]; then
 	# On Travis-CI, emit fold indicators
 	echo "travis_fold:start:zip.warzone"
 fi
+echo "Creating Warzone.zip..."
 if ! zip -r Warzone.zip -qdgds 10m Warzone.app; then
 	exit ${?}
 fi
+echo "Warzone.zip created."
 if [ -n "$TRAVIS" ]; then
 	# On Travis-CI, emit fold indicators
 	echo "travis_fold:end:zip.warzone"
 fi
-echo "Warzone.zip created."
 cd ../..
 
 cd ..
