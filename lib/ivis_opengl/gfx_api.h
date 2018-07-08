past-due/warzone2100

commit ac08585f245b1dd36bbd8d0a9f88b2352418f845

Wrap texture object.

lib/ivis_opengl/gfx_api.h
@@ -1,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+namespace gfx_api
+{
+	enum class pixel_format
+	{
+		invalid,
+		rgb,
+		rgba,
+		compressed_rgb,
+		compressed_rgba,
+	};
+
+	struct texture
+	{
+		virtual ~texture() {};
+		virtual void bind() = 0;
+		virtual void upload(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t& width, const size_t& height, const pixel_format& buffer_format, const void* data) = 0;
+		virtual void generate_mip_levels() = 0;
+		virtual unsigned id() = 0;
+	};
+
+	struct context
+	{
+		virtual ~context() {};
+		virtual texture* create_texture(const size_t& width, const size_t& height, const pixel_format& internal_format, const std::string& filename = "") = 0;
+		static context& get();
+	};
+}
+

lib/ivis_opengl/gfx_api_gl.cpp
@@ -1,0 +1,87 @@
+#include "gfx_api.h"
+
+#include <GL/glew.h>
+#include <algorithm>
+#include <cmath>
+#include "lib/framework/frame.h"
+
+static GLenum to_gl(const gfx_api::pixel_format& format)
+{
+	switch (format)
+	{
+	case gfx_api::pixel_format::rgba:
+		return GL_RGBA;
+	case gfx_api::pixel_format::rgb:
+		return GL_RGB;
+	case gfx_api::pixel_format::compressed_rgb:
+		return GL_RGB_S3TC;
+	case gfx_api::pixel_format::compressed_rgba:
+		return GL_RGBA_S3TC;
+	default:
+		debug(LOG_FATAL, "Unrecognised pixel format");
+	}
+	return GL_INVALID_ENUM;
+}
+
+struct gl_texture : public gfx_api::texture
+{
+private:
+	friend struct gl_context;
+	GLuint _id;
+
+	gl_texture()
+	{
+		glGenTextures(1, &_id);
+	}
+
+	~gl_texture()
+	{
+		glDeleteTextures(1, &_id);
+	}
+public:
+	virtual void bind() override
+	{
+		glBindTexture(GL_TEXTURE_2D, _id);
+	}
+
+	virtual void upload(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t & width, const size_t & height, const gfx_api::pixel_format & buffer_format, const void * data) override
+	{
+		bind();
+		glTexSubImage2D(GL_TEXTURE_2D, mip_level, offset_x, offset_y, width, height, to_gl(buffer_format), GL_UNSIGNED_BYTE, data);
+	}
+
+	virtual unsigned id() override
+	{
+		return _id;
+	}
+
+	virtual void generate_mip_levels() override
+	{
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+
+};
+
+struct gl_context : public gfx_api::context
+{
+	virtual gfx_api::texture* create_texture(const size_t & width, const size_t & height, const gfx_api::pixel_format & internal_format, const std::string& filename) override
+	{
+		auto* new_texture = new gl_texture();
+		new_texture->bind();
+		if (!filename.empty() && (GLEW_VERSION_4_3 || GLEW_KHR_debug))
+		{
+			glObjectLabel(GL_TEXTURE, new_texture->id(), -1, filename.c_str());
+		}
+		for (unsigned i = 0; i < floor(log(std::max(width, height))); ++i)
+		{
+			glTexImage2D(GL_TEXTURE_2D, i, to_gl(internal_format), width >> i, height >> i, 0, to_gl(internal_format), GL_UNSIGNED_BYTE, nullptr);
+		}
+		return new_texture;
+	}
+};
+
+gfx_api::context& gfx_api::context::get()
+{
+	static gl_context ctx;
+	return ctx;
+}

lib/ivis_opengl/pieblitfunc.cpp
@@ -74,10 +74,6 @@ static Vector2i makePieImage(IMAGEFILE *imageFile, unsigned id, PIERECT *dest = 
 GFX::GFX(GFXTYPE type, GLenum drawType, int coordsPerVertex) : mType(type), mdrawType(drawType), mCoordsPerVertex(coordsPerVertex), mSize(0)
 {
 	glGenBuffers(VBO_MINIMAL, mBuffers);
-	if (type == GFX_TEXTURE)
-	{
-		glGenTextures(1, &mTexture);
-	}
 }
 
 void GFX::loadTexture(const char *filename, GLenum filter)
@@ -97,12 +93,15 @@ void GFX::loadTexture(const char *filename, GLenum filter)
 	}
 }
 
-void GFX::makeTexture(int width, int height, GLenum filter, GLenum format, const GLvoid *image)
+void GFX::makeTexture(int width, int height, GLenum filter, const gfx_api::pixel_format& format, const GLvoid *image)
 {
 	ASSERT(mType == GFX_TEXTURE, "Wrong GFX type");
 	pie_SetTexturePage(TEXPAGE_EXTERN);
-	glBindTexture(GL_TEXTURE_2D, mTexture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, format, GL_UNSIGNED_BYTE, image);
+	if (mTexture)
+		delete mTexture;
+	mTexture = gfx_api::context::get().create_texture(width, height, format);
+	if (image != nullptr)
+		mTexture->upload(0u, 0u, 0u, width, height, format, image);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -124,8 +123,7 @@ void GFX::updateTexture(const void *image, int width, int height)
 		height = mHeight;
 	}
 	pie_SetTexturePage(TEXPAGE_EXTERN);
-	glBindTexture(GL_TEXTURE_2D, mTexture);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, mFormat, GL_UNSIGNED_BYTE, image);
+	mTexture->upload(0u, 0u, 0u, width, height, mFormat, image);
 }
 
 void GFX::buffers(int vertices, const GLvoid *vertBuf, const GLvoid *auxBuf)
@@ -156,7 +154,7 @@ void GFX::draw(const glm::mat4 &modelViewProjectionMatrix)
 	if (mType == GFX_TEXTURE)
 	{
 		pie_SetTexturePage(TEXPAGE_EXTERN);
-		glBindTexture(GL_TEXTURE_2D, mTexture);
+		mTexture->bind();
 		pie_ActivateShader(SHADER_GFX_TEXT, modelViewProjectionMatrix, glm::vec4(1), 0);
 		glEnableVertexAttribArray(VERTEX_COORDS_ATTRIB_INDEX);
 		glBindBuffer(GL_ARRAY_BUFFER, mBuffers[VBO_TEXCOORD]);
@@ -190,10 +188,8 @@ void GFX::draw(const glm::mat4 &modelViewProjectionMatrix)
 GFX::~GFX()
 {
 	glDeleteBuffers(VBO_MINIMAL, mBuffers);
-	if (mType == GFX_TEXTURE)
-	{
-		glDeleteTextures(1, &mTexture);
-	}
+	if (mTexture)
+		delete mTexture;
 }
 
 static void enableRect()
@@ -370,11 +366,11 @@ void iV_DrawImage(GLuint TextureID, Vector2i Position, Vector2i offset, Vector2i
 	iv_DrawImageImpl(offset, size, Vector2f(0.f, 0.f), Vector2f(1.f, 1.f), colour, mvp);
 }
 
-void iV_DrawImageText(GLuint TextureID, Vector2i Position, Vector2i offset, Vector2i size, float angle, REND_MODE mode, PIELIGHT colour)
+void iV_DrawImageText(gfx_api::texture& TextureID, Vector2i Position, Vector2i offset, Vector2i size, float angle, REND_MODE mode, PIELIGHT colour)
 {
 	pie_SetRendMode(mode);
 	pie_SetTexturePage(TEXPAGE_EXTERN);
-	glBindTexture(GL_TEXTURE_2D, TextureID);
+	TextureID.bind();
 
 	glm::mat4 mvp = defaultProjectionMatrix() * glm::translate(Position.x, Position.y, 0) * glm::rotate(angle, glm::vec3(0.f, 0.f, 1.f));
 

lib/ivis_opengl/piemode.cpp
@@ -53,15 +53,13 @@ bool pie_Initialise()
 	pie_TexInit();
 
 	/* Find texture compression extension */
-	if (GLEW_ARB_texture_compression && wz_texture_compression != GL_RGBA)
+	if (GLEW_ARB_texture_compression && wz_texture_compression)
 	{
 		debug(LOG_TEXTURE, "Texture compression: Yes");
-		wz_texture_compression = GL_COMPRESSED_RGBA_ARB;
 	}
 	else
 	{
 		debug(LOG_TEXTURE, "Texture compression: No");
-		wz_texture_compression = GL_RGBA;
 	}
 
 	rendSurface.width	= pie_GetVideoBufferWidth();

lib/ivis_opengl/screen.cpp
@@ -54,7 +54,7 @@
 #include <glm/gtx/transform.hpp>
 
 /* global used to indicate preferred internal OpenGL format */
-int wz_texture_compression = 0;
+bool wz_texture_compression = 0;
 
 // for compatibility with older versions of GLEW
 #ifndef GLEW_ARB_timer_query
@@ -537,7 +537,7 @@ void screen_Upload(const char *newBackDropBmp)
 
 	if (newBackDropBmp) // preview
 	{
-		backdropGfx->makeTexture(BACKDROP_HACK_WIDTH, BACKDROP_HACK_HEIGHT, GL_NEAREST, GL_RGB, newBackDropBmp);
+		backdropGfx->makeTexture(BACKDROP_HACK_WIDTH, BACKDROP_HACK_HEIGHT, GL_NEAREST, gfx_api::pixel_format::rgb, newBackDropBmp);
 
 		int s1 = screenWidth / preview_width;
 		int s2 = screenHeight / preview_height;

lib/ivis_opengl/tex.cpp
@@ -18,9 +18,7 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-
 #include "lib/framework/frame.h"
-#include "lib/framework/opengl.h"
 
 #include "lib/ivis_opengl/ivisdef.h"
 #include "lib/ivis_opengl/piestate.h"
@@ -36,16 +34,31 @@
 struct iTexPage
 {
 	char name[iV_TEXNAME_MAX];
-	GLuint id;
+	gfx_api::texture* id = nullptr;
+
+	iTexPage() = default;
+
+	iTexPage(iTexPage&& input)
+	{
+		memcpy(name, input.name, iV_TEXNAME_MAX);
+		id = input.id;
+		input.id = nullptr;
+	}
+
+	~iTexPage()
+	{
+		if (id)
+			delete id;
+	}
 };
 
-QList<iTexPage> _TEX_PAGE;
+std::vector<iTexPage> _TEX_PAGE;
 
 //*************************************************************************
 
-GLuint pie_Texture(int page)
+gfx_api::texture& pie_Texture(int page)
 {
-	return _TEX_PAGE[page].id;
+	return *(_TEX_PAGE[page].id);
 }
 
 int pie_NumberOfPages()
@@ -54,13 +67,19 @@ int pie_NumberOfPages()
 }
 
 // Add a new texture page to the list
-int pie_ReserveTexture(const char *name)
+int pie_ReserveTexture(const char *name, const size_t& width, const size_t& height)
 {
 	iTexPage tex;
-	glGenTextures(1, &tex.id);
 	sstrcpy(tex.name, name);
-	_TEX_PAGE.append(tex);
+	_TEX_PAGE.push_back(std::move(tex));
 	return _TEX_PAGE.size() - 1;
+}
+
+void pie_AssignTexture(int page, gfx_api::texture* texture)
+{
+	if (_TEX_PAGE[page].id)
+		delete _TEX_PAGE[page].id;
+	_TEX_PAGE[page].id = texture;
 }
 
 int pie_AddTexPage(iV_Image *s, const char *filename, bool gameTexture, int page)
@@ -71,9 +90,8 @@ int pie_AddTexPage(iV_Image *s, const char *filename, bool gameTexture, int page
 	{
 		iTexPage tex;
 		page = _TEX_PAGE.size();
-		glGenTextures(1, &tex.id);
 		sstrcpy(tex.name, filename);
-		_TEX_PAGE.append(tex);
+		_TEX_PAGE.push_back(std::move(tex));
 	}
 	else // replace
 	{
@@ -82,22 +100,36 @@ int pie_AddTexPage(iV_Image *s, const char *filename, bool gameTexture, int page
 	}
 	debug(LOG_TEXTURE, "%s page=%d", filename, page);
 
-	pie_SetTexturePage(page);
-	if (GLEW_VERSION_4_3 || GLEW_KHR_debug)
-	{
-		glObjectLabel(GL_TEXTURE, pie_Texture(page), -1, filename);
-	}
-
 	if (gameTexture) // this is a game texture, use texture compression
 	{
-		gluBuild2DMipmaps(GL_TEXTURE_2D, wz_texture_compression, s->width, s->height, iV_getPixelFormat(s), GL_UNSIGNED_BYTE, s->bmp);
+		gfx_api::pixel_format format{};
+		switch (iV_getPixelFormat(s))
+		{
+		case gfx_api::pixel_format::rgba:
+			format = wz_texture_compression ? gfx_api::pixel_format::compressed_rgba : gfx_api::pixel_format::rgba;
+			break;
+		case gfx_api::pixel_format::rgb:
+			format = wz_texture_compression ? gfx_api::pixel_format::compressed_rgb : gfx_api::pixel_format::rgb;
+			break;
+		default:
+			debug(LOG_FATAL, "getPixelFormat only returns rgb or rgba at the moment");
+		}
+		if (_TEX_PAGE[page].id)
+			delete _TEX_PAGE[page].id;
+		_TEX_PAGE[page].id = gfx_api::context::get().create_texture(s->width, s->height, format, filename);
+		pie_Texture(page).upload(0u, 0u, 0u, s->width, s->height, iV_getPixelFormat(s), s->bmp);
+		pie_Texture(page).generate_mip_levels();
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	}
 	else	// this is an interface texture, do not use compression
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, s->width, s->height, 0, iV_getPixelFormat(s), GL_UNSIGNED_BYTE, s->bmp);
+		if (_TEX_PAGE[page].id)
+			delete _TEX_PAGE[page].id;
+		_TEX_PAGE[page].id = gfx_api::context::get().create_texture(s->width, s->height, gfx_api::pixel_format::rgba, filename);
+		pie_Texture(page).upload(0u, 0u, 0u, s->width, s->height, iV_getPixelFormat(s), s->bmp);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	}
+	pie_SetTexturePage(page);
 	// it is uploaded, we do not need it anymore
 	free(s->bmp);
 	s->bmp = nullptr;
@@ -211,7 +243,7 @@ bool replaceTexture(const QString &oldfile, const QString &newfile)
 		if (strcmp(tmpname, _TEX_PAGE[i].name) == 0)
 		{
 			GL_DEBUG("Replacing texture");
-			debug(LOG_TEXTURE, "Replacing texture %s with %s from index %d (tex id %u)", _TEX_PAGE[i].name, newfile.toUtf8().constData(), i, _TEX_PAGE[i].id);
+			debug(LOG_TEXTURE, "Replacing texture %s with %s from index %d (tex id %u)", _TEX_PAGE[i].name, newfile.toUtf8().constData(), i, _TEX_PAGE[i].id->id());
 			sstrcpy(tmpname, newfile.toUtf8().constData());
 			pie_MakeTexPageName(tmpname);
 			pie_AddTexPage(&image, tmpname, true, i);
@@ -227,12 +259,7 @@ bool replaceTexture(const QString &oldfile, const QString &newfile)
 void pie_TexShutDown()
 {
 	// TODO, lazy deletions for faster loading of next level
-	debug(LOG_TEXTURE, "Cleaning out %u textures", _TEX_PAGE.size());
-	int _TEX_INDEX = _TEX_PAGE.size() - 1;
-	while (_TEX_INDEX > 0)
-	{
-		glDeleteTextures(1, &_TEX_PAGE[_TEX_INDEX--].id);
-	}
+	debug(LOG_TEXTURE, "Cleaning out %u textures", static_cast<unsigned>(_TEX_PAGE.size()));
 	_TEX_PAGE.clear();
 }
 
@@ -257,16 +284,16 @@ void iV_unloadImage(iV_Image *image)
 	}
 }
 
-unsigned int iV_getPixelFormat(const iV_Image *image)
+gfx_api::pixel_format iV_getPixelFormat(const iV_Image *image)
 {
 	switch (image->depth)
 	{
 	case 3:
-		return GL_RGB;
+		return gfx_api::pixel_format::rgb;
 	case 4:
-		return GL_RGBA;
+		return gfx_api::pixel_format::rgba;
 	default:
-		debug(LOG_ERROR, "iV_getPixelFormat: Unsupported image depth: %u", image->depth);
-		return GL_INVALID_ENUM;
+		debug(LOG_FATAL, "iV_getPixelFormat: Unsupported image depth: %u", image->depth);
+		return gfx_api::pixel_format::invalid;
 	}
 }

lib/ivis_opengl/textdraw.cpp
@@ -430,20 +430,10 @@ static FTFace &getFTFace(iV_fonts FontID)
 	}
 }
 
-static GLuint textureID = 0;
-static GLuint pbo = 0;
+static gfx_api::texture* textureID = nullptr;
 
 void iV_TextInit()
 {
-	pie_SetTexturePage(TEXPAGE_EXTERN);
-	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glGenBuffers(1, &pbo);
 	regular = new FTFace(getGlobalFTlib().lib, "fonts/DejaVuSans.ttf", 12 * 64, DPI, DPI);
 	bold = new FTFace(getGlobalFTlib().lib, "fonts/DejaVuSans-Bold.ttf", 21 * 64, DPI, DPI);
 	medium = new FTFace(getGlobalFTlib().lib, "fonts/DejaVuSans.ttf", 16 * 64, DPI, DPI);
@@ -464,8 +454,8 @@ void iV_TextShutdown()
 	bold = nullptr;
 	small = nullptr;
 	smallBold = nullptr;
-	glDeleteBuffers(1, &pbo);
-	glDeleteTextures(1, &textureID);
+	delete textureID;
+	textureID = nullptr;
 }
 
 unsigned int iV_GetTextWidth(const char *string, iV_fonts fontID)
@@ -690,13 +680,16 @@ void iV_DrawTextRotated(const char *string, float XPos, float YPos, float rotati
 	if (width > 0 && height > 0)
 	{
 		pie_SetTexturePage(TEXPAGE_EXTERN);
-		glBindTexture(GL_TEXTURE_2D, textureID);
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
-		glBufferData(GL_PIXEL_UNPACK_BUFFER, 4 * width * height, texture.get(), GL_STREAM_DRAW);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+		if (textureID)
+			delete textureID;
+		textureID = gfx_api::context::get().create_texture(width, height, gfx_api::pixel_format::rgba);
+		textureID->upload(0u, 0u, 0u, width, height, gfx_api::pixel_format::rgba, texture.get());
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glDisable(GL_CULL_FACE);
-		iV_DrawImageText(textureID, Vector2i(XPos, YPos), Vector2i(xoffset, yoffset), Vector2i(width, height), rotation, REND_TEXT, color);
+		iV_DrawImageText(*textureID, Vector2i(XPos, YPos), Vector2i(xoffset, yoffset), Vector2i(width, height), rotation, REND_TEXT, color);
 		glEnable(GL_CULL_FACE);
 	}
 }
@@ -744,16 +737,6 @@ void WzText::setText(const std::string &string, iV_fonts fontID)
 	}
 	mFontID = fontID;
 	mText = string;
-	if (texture == 0)
-	{
-		pie_SetTexturePage(TEXPAGE_EXTERN);
-		glGenTextures(1, &texture);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	}
 	TextRun tr(string, "en", HB_SCRIPT_COMMON, HB_DIRECTION_LTR);
 	std::unique_ptr<unsigned char[]> data;
 	FTFace &face = getFTFace(fontID);
@@ -762,11 +745,14 @@ void WzText::setText(const std::string &string, iV_fonts fontID)
 	if (dimensions.x > 0 && dimensions.y > 0)
 	{
 		pie_SetTexturePage(TEXPAGE_EXTERN);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
-		glBufferData(GL_PIXEL_UNPACK_BUFFER, 4 * dimensions.x * dimensions.y, data.get(), GL_STREAM_DRAW);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, dimensions.x, dimensions.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+		if (texture)
+			delete texture;
+		texture = gfx_api::context::get().create_texture(dimensions.x, dimensions.y, gfx_api::pixel_format::rgba);
+		texture->upload(0u, 0u, 0u, dimensions.x , dimensions.y, gfx_api::pixel_format::rgba, data.get());
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
 		mAboveBase = -(type->size->metrics.ascender >> 6);
 		mLineSize = (type->size->metrics.ascender - type->size->metrics.descender) >> 6;
@@ -782,17 +768,18 @@ WzText::WzText(const std::string &string, iV_fonts fontID)
 
 WzText::~WzText()
 {
-	glDeleteTextures(1, &texture); // ok to call with texture ID zero
+	if (texture)
+		delete texture;
 }
 
 void WzText::render(Vector2i position, PIELIGHT colour, float rotation)
 {
-	ASSERT(texture != 0, "Text not initialized before rendering");
+	ASSERT(texture != 0 || dimensions.x == 0 || dimensions.y == 0, "Text not initialized before rendering");
 	if (rotation != 0.f)
 	{
 		rotation = 180. - rotation;
 	}
 	glDisable(GL_CULL_FACE);
-	iV_DrawImageText(texture, position, offsets, dimensions, rotation, REND_TEXT, colour);
+	iV_DrawImageText(*texture, position, offsets, dimensions, rotation, REND_TEXT, colour);
 	glEnable(GL_CULL_FACE);
 }

commit 076aaac632e2677b32953e06b04752fb26905a83

Fix OpenGL texture compression support
Texture compression was broken by: ac08585f245b1dd36bbd8d0a9f88b2352418f845

(However, the above commit *also* changed the default state for texture compression from enabled to disabled.)

This commit also substantially reduces the number of texture memory allocations by modifying the OpenGL implementation of gfx_api::context::get().create_texture (it no longer allocates a bunch of texture memory for OpenGL up-front), and following a pattern of:
- gfx_api::context::get().create_texture
Followed by:
- gfx_api::texture::upload (which, for OpenGL, allocates the texture memory)
Before any calls to the new:
- gfx_api::texture::update_subrect (which modifies an existing rect of texture memory)

lib/ivis_opengl/gfx_api.h
@@ -18,7 +18,8 @@ namespace gfx_api
 	{
 		virtual ~texture() {};
 		virtual void bind() = 0;
-		virtual void upload(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t& width, const size_t& height, const pixel_format& buffer_format, const void* data) = 0;
+		virtual void upload(const size_t& mip_level, const size_t& width, const size_t& height, const pixel_format& buffer_format, const void* data) = 0;
+		virtual void update_subrect(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t& width, const size_t& height, const pixel_format& buffer_format, const void* data) = 0;
 		virtual void generate_mip_levels() = 0;
 		virtual unsigned id() = 0;
 	};

lib/ivis_opengl/gfx_api_gl.cpp
@@ -14,9 +14,9 @@ static GLenum to_gl(const gfx_api::pixel_format& format)
 	case gfx_api::pixel_format::rgb:
 		return GL_RGB;
 	case gfx_api::pixel_format::compressed_rgb:
-		return GL_RGB_S3TC;
+		return GL_COMPRESSED_RGB_ARB;
 	case gfx_api::pixel_format::compressed_rgba:
-		return GL_RGBA_S3TC;
+		return GL_COMPRESSED_RGBA_ARB;
 	default:
 		debug(LOG_FATAL, "Unrecognised pixel format");
 	}
@@ -28,8 +28,10 @@ struct gl_texture : public gfx_api::texture
 private:
 	friend struct gl_context;
 	GLuint _id;
+	const gfx_api::pixel_format _internal_format;
 
-	gl_texture()
+	gl_texture(const gfx_api::pixel_format & internal_format)
+		: _internal_format(internal_format)
 	{
 		glGenTextures(1, &_id);
 	}
@@ -44,7 +46,13 @@ public:
 		glBindTexture(GL_TEXTURE_2D, _id);
 	}
 
-	virtual void upload(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t & width, const size_t & height, const gfx_api::pixel_format & buffer_format, const void * data) override
+	virtual void upload(const size_t& mip_level, const size_t & width, const size_t & height, const gfx_api::pixel_format & buffer_format, const void * data) override
+	{
+		bind();
+		glTexImage2D(GL_TEXTURE_2D, mip_level, to_gl(_internal_format), width, height, 0, to_gl(buffer_format), GL_UNSIGNED_BYTE, data);
+	}
+
+	virtual void update_subrect(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t & width, const size_t & height, const gfx_api::pixel_format & buffer_format, const void * data) override
 	{
 		bind();
 		glTexSubImage2D(GL_TEXTURE_2D, mip_level, offset_x, offset_y, width, height, to_gl(buffer_format), GL_UNSIGNED_BYTE, data);
@@ -66,15 +74,11 @@ struct gl_context : public gfx_api::context
 {
 	virtual gfx_api::texture* create_texture(const size_t & width, const size_t & height, const gfx_api::pixel_format & internal_format, const std::string& filename) override
 	{
-		auto* new_texture = new gl_texture();
+		auto* new_texture = new gl_texture(internal_format);
 		new_texture->bind();
 		if (!filename.empty() && (GLEW_VERSION_4_3 || GLEW_KHR_debug))
 		{
 			glObjectLabel(GL_TEXTURE, new_texture->id(), -1, filename.c_str());
-		}
-		for (unsigned i = 0; i < floor(log(std::max(width, height))) + 1; ++i)
-		{
-			glTexImage2D(GL_TEXTURE_2D, i, to_gl(internal_format), width >> i, height >> i, 0, to_gl(internal_format), GL_UNSIGNED_BYTE, nullptr);
 		}
 		return new_texture;
 	}

lib/ivis_opengl/pieblitfunc.cpp
@@ -99,8 +99,7 @@ void GFX::makeTexture(int width, int height, GLenum filter, const gfx_api::pixel
 	if (mTexture)
 		delete mTexture;
 	mTexture = gfx_api::context::get().create_texture(width, height, format);
-	if (image != nullptr)
-		mTexture->upload(0u, 0u, 0u, width, height, format, image);
+	mTexture->upload(0u, width, height, format, image);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -122,7 +121,7 @@ void GFX::updateTexture(const void *image, int width, int height)
 		height = mHeight;
 	}
 	pie_SetTexturePage(TEXPAGE_EXTERN);
-	mTexture->upload(0u, 0u, 0u, width, height, mFormat, image);
+	mTexture->update_subrect(0u, 0u, 0u, width, height, mFormat, image);
 }
 
 void GFX::buffers(int vertices, const GLvoid *vertBuf, const GLvoid *auxBuf)

lib/ivis_opengl/piemode.cpp
@@ -71,7 +71,9 @@ bool pie_Initialise()
 	}
 	else
 	{
+		// Texture compression is not supported
 		debug(LOG_TEXTURE, "Texture compression: No");
+		wz_texture_compression = false;
 	}
 
 	pie_UpdateSurfaceGeometry();

lib/ivis_opengl/screen.cpp
@@ -56,8 +56,8 @@
 #endif
 #include <glm/gtx/transform.hpp>
 
-/* global used to indicate preferred internal OpenGL format */
-bool wz_texture_compression = 0;
+/* global used to indicate whether the internal texture format should be compressed (if supported) */
+bool wz_texture_compression = true;
 
 // for compatibility with older versions of GLEW
 #ifndef GLEW_ARB_timer_query

lib/ivis_opengl/tex.cpp
@@ -81,6 +81,11 @@ void pie_AssignTexture(int page, gfx_api::texture* texture)
 	_TEX_PAGE[page].id = texture;
 }
 
+inline bool IsPowerOfTwo(unsigned long x)
+{
+	return (x != 0) && ((x & (x - 1)) == 0);
+}
+
 int pie_AddTexPage(iV_Image *s, const char *filename, bool gameTexture, int page)
 {
 	ASSERT(s && filename, "Bad input parameter");
@@ -116,7 +121,8 @@ int pie_AddTexPage(iV_Image *s, const char *filename, bool gameTexture, int page
 		if (_TEX_PAGE[page].id)
 			delete _TEX_PAGE[page].id;
 		_TEX_PAGE[page].id = gfx_api::context::get().create_texture(s->width, s->height, format, filename);
-		pie_Texture(page).upload(0u, 0u, 0u, s->width, s->height, iV_getPixelFormat(s), s->bmp);
+		pie_Texture(page).upload(0u, s->width, s->height, iV_getPixelFormat(s), s->bmp);
+		ASSERT(IsPowerOfTwo(s->width) && IsPowerOfTwo(s->height), "Texture width and height (%u x %u) must be a power of two, or certain older graphics cards may not support generating mipmaps properly: %s", s->width, s->height, (filename) ? filename : "(no filename available)");
 		pie_Texture(page).generate_mip_levels();
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	}
@@ -125,7 +131,7 @@ int pie_AddTexPage(iV_Image *s, const char *filename, bool gameTexture, int page
 		if (_TEX_PAGE[page].id)
 			delete _TEX_PAGE[page].id;
 		_TEX_PAGE[page].id = gfx_api::context::get().create_texture(s->width, s->height, gfx_api::pixel_format::rgba, filename);
-		pie_Texture(page).upload(0u, 0u, 0u, s->width, s->height, iV_getPixelFormat(s), s->bmp);
+		pie_Texture(page).upload(0u, s->width, s->height, iV_getPixelFormat(s), s->bmp);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	}
 	pie_SetTexturePage(page);

lib/ivis_opengl/textdraw.cpp
@@ -742,7 +742,7 @@ void iV_DrawTextRotated(const char *string, float XPos, float YPos, float rotati
 		if (textureID)
 			delete textureID;
 		textureID = gfx_api::context::get().create_texture(width, height, gfx_api::pixel_format::rgba);
-		textureID->upload(0u, 0u, 0u, width, height, gfx_api::pixel_format::rgba, texture.get());
+		textureID->upload(0u, width, height, gfx_api::pixel_format::rgba, texture.get());
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, text_filtering);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, text_filtering);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -851,7 +851,7 @@ void WzText::drawAndCacheText(const std::string &string, iV_fonts fontID)
 	{
 		pie_SetTexturePage(TEXPAGE_EXTERN);
 		texture = gfx_api::context::get().create_texture(dimensions.x, dimensions.y, gfx_api::pixel_format::rgba);
-		texture->upload(0u, 0u, 0u, dimensions.x , dimensions.y, gfx_api::pixel_format::rgba, data.get());
+		texture->upload(0u, dimensions.x , dimensions.y, gfx_api::pixel_format::rgba, data.get());
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, text_filtering);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, text_filtering);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);

src/terrain.cpp
@@ -928,7 +928,7 @@ bool initTerrain()
 	lightmap_tex_num = gfx_api::context::get().create_texture(lightmapWidth, lightmapHeight, gfx_api::pixel_format::rgb);
 
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	lightmap_tex_num->upload(0, 0, 0, lightmapWidth, lightmapHeight, gfx_api::pixel_format::rgb, lightmapPixmap);
+	lightmap_tex_num->upload(0, lightmapWidth, lightmapHeight, gfx_api::pixel_format::rgb, lightmapPixmap);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -1284,7 +1284,7 @@ void drawTerrain(const glm::mat4 &mvp)
 		updateLightMap();
 
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-		lightmap_tex_num->upload(0, 0, 0, lightmapWidth, lightmapHeight, gfx_api::pixel_format::rgb, lightmapPixmap);
+		lightmap_tex_num->update_subrect(0, 0, 0, lightmapWidth, lightmapHeight, gfx_api::pixel_format::rgb, lightmapPixmap);
 	}
 
 	///////////////////////////////////

src/texture.cpp
@@ -92,6 +92,7 @@ static int newPage(const char *name, int level, int width, int height, int count
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mipmap_levels - 1);
 
 	// debug(LOG_TEXTURE, "newPage: glTexImage2D(page=%d, level=%d) opengl id=%u", texPage, level, _TEX_PAGE[texPage].id);
+	pie_Texture(texPage).upload(level, width, height, gfx_api::pixel_format::rgba, nullptr);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -217,7 +218,7 @@ bool texLoad(const char *fileName)
 				break;
 			}
 			// Insert into texture page
-			pie_Texture(texPage).upload(j, xOffset, yOffset, tile.width, tile.height, gfx_api::pixel_format::rgba, tile.bmp);
+			pie_Texture(texPage).update_subrect(j, xOffset, yOffset, tile.width, tile.height, gfx_api::pixel_format::rgba, tile.bmp);
 			free(tile.bmp);
 			if (i == mipmap_max) // dealing with main texture page; so register coordinates
 			{