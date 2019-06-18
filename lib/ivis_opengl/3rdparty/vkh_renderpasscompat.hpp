12087d4d696de5efa2c687e4238de61903283556
past-due <30942300+past-due@users.noreply.github.com>
2019-06-17 20:53:11 -0400

    MSVC Vulkan compile fixes (3)

--- lib/ivis_opengl/3rdparty/vkh_renderpasscompat.hpp
+++ lib/ivis_opengl/3rdparty/vkh_renderpasscompat.hpp
@@ -12,7 +12,14 @@
 
 #pragma once
 
+#if defined( _MSC_VER )
+#pragma warning( push )
+#pragma warning( disable : 4191 ) // warning C4191: '<function-style-cast>': unsafe conversion from 'PFN_vkVoidFunction' to 'PFN_vk<...>'
+#endif
 #include <vulkan/vulkan.hpp>
+#if defined( _MSC_VER )
+#pragma warning( pop )
+#endif
 
 #include <vector>
 #include <memory>
