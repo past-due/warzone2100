40a595c6e774ef89668279488147da93170ef85c
past-due <30942300+past-due@users.noreply.github.com>
2020-02-28 12:18:00 -0500

    Test 14

--- .ci/powershell/request.ps1
+++ .ci/powershell/request.ps1
@@ -1,3 +1,44 @@
+# See: https://github.com/PowerShell/PowerShell/issues/2896
+function Get-RedirectedUrl() {
+  param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [uri] $url,
+    [Parameter(Position = 1)]
+    [Microsoft.PowerShell.Commands.WebRequestSession] $session = $null
+  )
+
+  $request_url = $url
+  $retry = $false
+
+  do {
+    try {
+      $response = Invoke-WebRequest -Method Head -WebSession $session -Uri $request_url
+
+      if($response.BaseResponse.ResponseUri -ne $null)
+      {
+        # PowerShell 5
+        $result = $response.BaseResponse.ResponseUri.AbsoluteUri
+      } elseif ($response.BaseResponse.RequestMessage.RequestUri -ne $null) {
+        # PowerShell Core
+        $result = $response.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+      }
+
+      $retry = $false
+    } catch {
+      if(($_.Exception.GetType() -match "HttpResponseException") -and
+        ($_.Exception -match "302"))
+      {
+        $request_url = $_.Exception.Response.Headers.Location.AbsoluteUri
+        $retry = $true
+      } else {
+        throw $_
+      }
+    }  
+  } while($retry)
+
+  return $result
+}
+
 Function Req {
    Param(
        [Parameter(Mandatory=$True)]
