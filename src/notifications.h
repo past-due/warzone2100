7a833b28185f4e52dd7f7d0ae8556b8c2f6003de
past-due <30942300+past-due@users.noreply.github.com>
2020-03-03 22:08:52 -0500

    [WIP] Notification widget work - more cleanup 5

--- src/notifications.h
+++ src/notifications.h
@@ -1,7 +1,6 @@
 /*
 	This file is part of Warzone 2100.
-	Copyright (C) 1999-2004  Eidos Interactive
-	Copyright (C) 2005-2020  Warzone 2100 Project
+	Copyright (C) 2020  Warzone 2100 Project
 
 	Warzone 2100 is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -27,8 +26,6 @@
 #include <list>
 #include <memory>
 #include <functional>
-
-struct iV_Image;
 
 class WZ_Notification_Display_Options
 {
