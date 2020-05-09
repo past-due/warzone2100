4a78a40a0243961e3bfe4c992bed9ea9855544d6
past-due <30942300+past-due@users.noreply.github.com>
2020-05-08 23:31:26 -0400

    Revert "Tweak update-po.sh (4)"
    This reverts commit d01ec77249b86bb86b496f5c2daf808204cae3ec.

--- po/update-po.sh
+++ po/update-po.sh
@@ -1,24 +1,7 @@
-#!/bin/bash
-
-sourced=0
-if [ -n "$ZSH_EVAL_CONTEXT" ]; then
-  case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
-elif [ -n "$KSH_VERSION" ]; then
-  [ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] && sourced=1
-elif [ -n "$BASH_VERSION" ]; then
-  (return 0 2>/dev/null) && sourced=1
-else # All other shells: examine $0 for known shell binary filenames
-  # Detects `sh` and `dash`; add additional shell filenames as needed.
-  case ${0##*/} in sh|dash) sourced=1;; esac
-fi
+#!/bin/sh
 
 # We need to be in the working copy's root directory
-if [ $sourced -eq 0 ]; then
-	cd "`dirname "$0"`/.."
-else
-	# If sourced, current directory *must* be the working copy root
-	echo "Treating as working copy root: \"$(pwd)\""
-fi
+cd "`dirname "$0"`/.."
 
 echo "LC_ALL=${LC_ALL}"
 export LC_ALL=C
