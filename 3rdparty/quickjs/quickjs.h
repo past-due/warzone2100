f3f5d30c050b2b0bae9bec702b351a486e7ed4ed
past-due <30942300+past-due@users.noreply.github.com>
2020-10-31 18:31:30 -0400

    QuickJS Patch: MSVC Compatibility - Part 4

--- 3rdparty/quickjs/quickjs.h
+++ 3rdparty/quickjs/quickjs.h
@@ -40,7 +40,11 @@ extern "C" {
 #else
 #define js_likely(x)     (x)
 #define js_unlikely(x)   (x)
+#if defined(_MSC_VER)
+#define js_force_inline  __forceinline
+#else
 #define js_force_inline  inline
+#endif
 #define __js_printf_like(a, b)
 #endif
 
