6040c0839249e47bd109c00a22ea2bfe638e085a
past-due <30942300+past-due@users.noreply.github.com>
2020-12-31 16:58:50 -0500

    W_LABEL: Use WzCachedText

--- lib/widget/label.h
+++ lib/widget/label.h
@@ -26,11 +26,12 @@
 
 #include "widget.h"
 #include "widgbase.h"
+#include "paragraph.h"
 #include "lib/ivis_opengl/textdraw.h"
 #include <string>
 
 struct LabelDisplayCache {
-	std::vector<WzText> wzText;
+	std::vector<WzCachedText> wzText;
 };
 
 class W_LABEL : public WIDGET
@@ -47,6 +48,8 @@ public:
 	WzString getString() const override;
 	void setString(WzString string) override;
 	void setTip(std::string string) override;
+
+	void run(W_CONTEXT *) override;
 
 	// Sets a string for the label
 	// - line-wraps at max width
