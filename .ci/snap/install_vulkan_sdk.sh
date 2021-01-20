8b37c72a1c16c4947ded025a6238f8b49abae3ad
past-due <30942300+past-due@users.noreply.github.com>
2021-01-20 14:07:09 -0500

    Tweak 5

--- .ci/snap/install_vulkan_sdk.sh
+++ .ci/snap/install_vulkan_sdk.sh
@@ -5,27 +5,42 @@ TARGET_ARCH="$1"
 echo "[install_vulkan_sdk.sh]"
 echo "Target Arch: $TARGET_ARCH"
 
-# Add Vulkan SDK repo
-wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
-wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.148-bionic.list https://packages.lunarg.com/vulkan/1.2.148/lunarg-vulkan-1.2.148-bionic.list
-
 if [ "$TARGET_ARCH" = "amd64_DISABLED" ]; then
   # Install Vulkan SDK (binary package)
   echo "Installing Vulkan SDK"
 
+  # Add Vulkan SDK repo
+  wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
+  wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.148-bionic.list https://packages.lunarg.com/vulkan/1.2.148/lunarg-vulkan-1.2.148-bionic.list
   apt update
   apt install --yes vulkan-sdk
+
 else
-  # Compile Vulkan SDK from source
-  echo "Compiling Vulkan SDK"
+  # Compile the necessary components of the Vulkan SDK from source
+  echo "Compiling Vulkan SDK (selected components)"
 
   mkdir tmp_vulkan_sdk_build
   cd "tmp_vulkan_sdk_build"
 
-  apt-src update
-  apt-src --build install vulkan-sdk
+  # glslc
+  DEBIAN_FRONTEND=noninteractive apt-get -y cmake python3 lcov
 
+  # glslc - clone
+  echo "Fetching shaderc"
+  git clone https://github.com/google/shaderc shaderc
+  cd shaderc
+  ./utils/git-sync-deps
   cd ..
+
+  # glslc - cmake build + install
+  mkdir shaderc_build
+  cd shaderc_build
+  echo "Configuring shaderc"
+  cmake -GNinja -DSHADERC_SKIP_TESTS=ON -DCMAKE_BUILD_TYPE=Release ../shaderc/
+  echo "Compiling shaderc"
+  cmake --build . --target install
+  cd ..
+
   rm -rf "tmp_vulkan_sdk_build"
 fi
 
