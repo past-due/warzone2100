5a45e28e8c3fe1c04a52eeeeb2ceaab9015be578
past-due <30942300+past-due@users.noreply.github.com>
2022-01-25 16:09:31 -0500

    Tweak last (4)

--- .ci/vcpkg/overlay-ports/basisu/portfile.cmake
+++ .ci/vcpkg/overlay-ports/basisu/portfile.cmake
@@ -6,28 +6,17 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
-
-#vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/basisu)
-if (WIN32)
-    set(TOOL_NAME basisu.exe)
-else()
-    set(TOOL_NAME basisu)
-endif()
+vcpkg_cmake_install()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(COPY ${CURRENT_PACKAGES_DIR}/bin/${TOOL_NAME} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/basisu)
-
-vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/basisu)
+vcpkg_copy_tools(TOOL_NAMES basisu AUTO_CLEAN)
 
 # Remove unnecessary files
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/${TOOL_NAME})
 
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
