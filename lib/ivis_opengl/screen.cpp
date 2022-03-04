057517b89eea8e3be6ca7b925d7949be55b8597d
past-due <30942300+past-due@users.noreply.github.com>
2022-03-04 11:21:18 -0500

    Refactor pie_ScreenFlip

--- lib/ivis_opengl/screen.cpp
+++ lib/ivis_opengl/screen.cpp
@@ -583,7 +583,8 @@ void screen_FlipIfBackDropTransition()
 	static auto hadBackDrop = false;
 	if (hadBackDrop != screen_GetBackDrop())
 	{
-		pie_ScreenFlip(CLEAR_BLACK);
+		pie_ScreenFrameRenderEnd();
+		pie_ScreenFrameRenderBegin();
 		hadBackDrop = screen_GetBackDrop();
 	}
 }
