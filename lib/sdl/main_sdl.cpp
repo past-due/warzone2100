c4622a6560d9a3747389ba3912daba69a2ce063d
past-due <30942300+past-due@users.noreply.github.com>
2022-08-17 15:17:53 -0400

    [WIP] Emscripten support - handle shutdown cleanup

--- lib/sdl/main_sdl.cpp
+++ lib/sdl/main_sdl.cpp
@@ -2951,6 +2951,9 @@ static void handleActiveEvent(SDL_Event *event)
 }
 
 static SDL_Event event;
+#if defined(__EMSCRIPTEN__)
+std::function<void()> saved_onShutdown;
+#endif
 
 void wzEventLoopOneFrame(void* arg)
 {
@@ -2986,7 +2989,12 @@ void wzEventLoopOneFrame(void* arg)
 				bool *bContinue = static_cast<bool*>(arg);
 				*bContinue = false;
 #if defined(__EMSCRIPTEN__)
-				// FUTURE TODO: Actually trigger cleanup code?
+				// Actually trigger cleanup code
+				if (saved_onShutdown)
+				{
+					saved_onShutdown();
+				}
+				wzShutdown();
 				
 				// Stop Emscripten from calling the main loop
 				emscripten_cancel_main_loop();
@@ -3028,6 +3036,7 @@ void wzMainEventLoop(std::function<void()> onShutdown)
 
 	bool bContinue = true;
 #if defined(__EMSCRIPTEN__)
+	saved_onShutdown = onShutdown;
 	// Receives a function to call and some user data to provide it.
 	emscripten_set_main_loop_arg(wzEventLoopOneFrame, &bContinue, -1, true);
 #else
